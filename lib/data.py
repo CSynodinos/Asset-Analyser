8e6d0da2e7d8855db51f8859d035b477f617a041
Christos Synodinos <chris_sinodinos@outlook.com>
2022-06-13 15:11:37 +0100

    updated data module with better docs and code

--- lib/data.py
+++ lib/data.py
@@ -1,147 +1,119 @@
-import sqlite3
+import sqlite3, os, datetime, time
 import pandas as pd
-import os
-import datetime
-import time
 import yfinance as yf
+from lib.exceptions import DateError
 
 class data:
-    start: int
-        
-    def __init__(self, start):
-        """Access data through the Yahoo API and store them in an SQLite local database.
-        Example:
-        >>> from lib import data as dat
-        >>> stocks = dat.data(start = a_start_date)
-        >>> stocks.stocks(database = 'a_database.db', stck = list_of_stocks, today = True)"""
-        
+    """Access data through the Yahoo API and store them in an SQLite local database.
+    """
+
+    def __init__(self, start: datetime) -> None:
         self.start = start 
-    
-    def stocks(self, database, stck, today = True, year = "", month = "", day = ""):
-        """Get stock data from Yahoo and add them into an SQLite database.
-                
-        `database` is the SQLite database that the data will be saved at.
-        
-        `stck` can either be a list or a string of cryptocurrencies.
-        
-        If `today = True`, the stocks data will start from the given date taken from the initialization of the class, and end in today's date.
-        
-        Otherwise, an exact date will need to be given through the `year`, `month` and `day` parameters"""""  
-        
-        if today == True:
-            end_time = datetime.datetime.now().date().isoformat()   # today
-        elif today == False and year != "" and month != "" and day != "":
-            end_time = datetime.datetime(year, month, day)   # Choose end date
-        elif today == True and year != "" and month != "" and day != "":
-            end_time = datetime.datetime(year, month, day)   # Choose end date
-        else:
-            print("Error: Please insert a specific date or today = True if you want today's date")
-            exit()
-        
-        if os.path.isfile(database):
-                db = True
-        else:
-                db = False
-        
-        data_fetch(db = database, type = 'stock', currency = stck, begin = self.start, stop = end_time) # get stock data
-        
-        if db == True:
-            result = print('Stocks Database has been successfully updated!')
-        elif db == False:
-            result = print('Stocks Database has been successfully generated!')
-        
-        return result
-    
-    def crypto(self, database, crypt, today = True, year = "", month = "", day = ""):
-        """Get cryptocurrency data from Yahoo and add them into an SQLite database.
-        
-        `database` is the SQLite database that the data will be saved at.
-        
-        `crypt` can either be a list or a string of cryptocurrencies.
-        
-        If `today = True`, the cryptocurrency data will start from the given date taken from the initialization of the class, and end in today's date.
-        
-        Otherwise, an exact date will need to be given through the `year`, `month` and `day` parameters""""" 
-        
-        if today == True:
-            final = datetime.datetime.now().date().isoformat()   # today
-        elif today == False and year != "" and month != "" and day != "":
-            final = datetime.datetime(year, month, day)   # Choose end date
-        elif today == True and year != "" and month != "" and day != "":
-            final = datetime.datetime(year, month, day)
-        else:
-            print("Error: Please insert a specific date or today = True if you want today's date")
-            exit()
-        
-        data_fetch(db = database, type = 'cryptocurrency', currency = crypt, begin = self.start, stop = final) # get crypto data
-        
-        if os.path.isfile(database):
-                db = True
-        else:
-                db = False
 
-        if db == True:
-            result = print('Cryptocurrency Database has been successfully updated!')
-        elif db == False:
-            result = print('Cryptocurrency Database has been successfully generated!')
-        
-        return result    
+    @staticmethod
+    def SQLite_Query(database: str, table: str) -> pd.DataFrame:
+        """Access an SQLite database and query a table.
 
-def SQLite_Query(database, table):
-    """Function to access an SQLite database and read an entire table. Returns the table into a pandas dataframe."""
-    
-    con = sqlite3.connect(database)   # Read SQLite table into dataframe.
-    df = pd.read_sql_query("SELECT * from %s" %table, con)
-    con.close()
-    
-    return df
+        Args:
+            * `database` (str): Database name.
+            * `table` (str): Table name.
 
-def data_fetch(db, type, currency, begin, stop):
-    """Method that gets data from Yahoo.
-    
-    `db` is the name of the database that the data will be added.
-    
-    `type` is the type of data e.g. stock.
-    
-    `currency` can either be a list or string of the data for download from Yahoo e.g. AAPL
-    
-    `begin` is the start date for the data and `stop` is the end date."""
-    
-    engine = sqlite3.connect(db)
-    cur = engine.cursor()
-        
-    print('Connecting to Yahoo Finance...')
-    
-    for i in currency:
+        Returns:
+            `pd.DataFrame`: Pandas Dataframe with the queried data.
+        """
+
+        con = sqlite3.connect(database)   # Read SQLite table into dataframe.
+        df = pd.read_sql_query("SELECT * from %s" %table, con)
+        con.close()
+        return df
+
+    @staticmethod
+    def data_fetch(db: str, type: str, currency: list, begin: str, stop: str) -> bool:
+        """Get data from Yahoo.
+
+        Args:
+            * `db` (str): Database name used for storage.
+            * `type` (str): Asset type e.g. stock.
+            * `currency` (list): List of assets.
+            * `begin` (str): Start date for data fetching.
+            * `stop` (str): End date for data fetching.
+
+        Returns:
+            Boolean: True when operation finishes successfully.
+        """
+
+        engine = sqlite3.connect(db)
+        cur = engine.cursor()
+        print('Connecting to Yahoo Finance...')
+        for i in currency:
             connected = False
             while not connected:    # Check connection to Yahoo finance.
                 try:
-                    print('Fetching %s %s data...' %(i, type))
+                    print(f'Fetching {i} {type} data...')
                     df = yf.download(tickers = i, start = begin, end = stop)
                     connected = True
                 except Exception as e:
                     print("type error: " + str(e))
                     time.sleep(30)
-                    pass    
-            
-            print('Adding %s data to %s database...' %(i, db))                
-             
-            df.rename(columns = {"Adj Close": "Adj_Close"}, inplace = True) # Replace white space with _ in column names.
-   
-            df = df.reset_index()   # Numerical integer index instead of date index.
-            tables = ('%s' %i)
-                    
-            str_to_replace = "-"
-            str_check = any(tables in str_to_replace for tables in tables)
-            
-            if str_check == True:  # Checks if the "-" character exists. If yes, then it gets replaced with "_" (Needed for crypto in SQL database).
-                crypto_table = tables.replace("-", "_")
-                df.to_sql(crypto_table, con = engine, if_exists='replace', index = True)
-            else:
-                df.to_sql(tables, con = engine, if_exists='replace', index = True)
-                
-            print('%s %s data saved!\n' %(i, type))
 
-    # close db session.
-    cur.close()
-    engine.close()
+                print(f'Adding {i} data to {db} database...')                
+
+                df.rename(columns = {"Adj Close": "Adj_Close"}, inplace = True) # Replace white space with _ in column names.
+
+                df = df.reset_index()   # Numerical integer index instead of date index.
+
+                str_to_replace = "-"
+                str_check = any(tables in str_to_replace for tables in i)
+
+                if str_check == True:  # Checks if the "-" character exists. If yes, then it gets replaced with "_" (Needed for crypto in SQL database).
+                    crypto_table = i.replace("-", "_")
+                    df.to_sql(crypto_table, con = engine, if_exists='replace', index = True)
+                else:
+                    df.to_sql(i, con = engine, if_exists='replace', index = True)
+                print(f'{i} {type} data saved!\n')
+
+        cur.close()
+        engine.close()
+
+        return True
+
+    def asset_data(self, database: str, asset_type: str, asset_list: list, today = True, 
+                year = None, month = None, day = None) -> bool:
+        """Get asset data and append them into an SQLite database.
+
+        Args:
+            * `database` (str): SQLite database that the data will be saved at.
+            * `asset_type` (str): Asset type e.g. stock.
+            * `asset_list` (list): List of assets.
+            * `today` (bool): Select today's date.
+
+        Raises:
+            DateError: When data is incorrectly selected.
+
+        Returns:
+            Boolean: True when operation finishes successfully.
+        """
+
+        if today == True:
+            end_time = datetime.datetime.now().date().isoformat()   # today
+        elif today == False and (year, month, day) != None:
+            end_time = datetime.datetime(year, month, day)   # Choose end date
+        elif today == True and (year, month, day) != None:
+            raise DateError("today = True and year, month, day are set. Please set one of the two types of dates.")
+        else:
+            raise DateError("Please insert a specific date or today = True if you want today's date")
+
+        if os.path.isfile(database):
+            db = True
+        else:
+            db = False
+
+        self.data_fetch(db = database, type = asset_type, currency = asset_list,
+                        begin = self.start, stop = end_time) # get stock data
+
+        if db == True:
+            print(f'{asset_type} Database has been successfully updated!')
+        elif db == False:
+            print(f'{asset_type} Database has been successfully generated!')
+
+        return True
