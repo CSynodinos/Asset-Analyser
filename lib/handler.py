b25156823d53cb91e179b4d93209013dca6f9f9a
Christos Synodinos <chris_sinodinos@outlook.com>
2022-06-13 15:57:50 +0100

    __repr__ and __dir__ for directories class

--- lib/handler.py
+++ lib/handler.py
@@ -1,25 +1,48 @@
-import os
-import sys
-import fnmatch
+import os, sys, fnmatch
+from inspect import getfullargspec
 
-class directories():    
+class directories:
+    """Class holds 3 functions:
+
+    >>> OS()
+
+    >>> location(pattern)
+
+    >>> contents(dir, pattern, extension)
+
+    OS() finds the operating system currently running in the machine.
+
+    location() finds a subdirectory according to a given pattern.
+
+    contents() finds a file according to a given pattern.
+    """    
+
     def __init__(self, d):
-        """Class holds 3 functions:
-    
-        >>> OS()
-        
-        >>> location(pattern)
-        
-        >>> contents(dir, pattern, extension)
-        
-        OS( ) finds the operating system currently running in the machine.
-        
-        location( ) finds a subdirectory according to a given pattern.
-        
-        contents( ) finds a file according to a given pattern."""
-        
         self.d = d
-    
+
+    @classmethod
+    def __repr__(cls) -> str:
+        params = getfullargspec(__class__).args
+        params.remove("self")
+        return params
+
+    @classmethod
+    def __dir__(cls, only_added = False) -> list:
+        """Display function attributes.
+        Args:
+            * `only_added` (bool, optional): Choose whether to display only the specified attributes. Defaults to False.
+        Returns:
+            list: List of attributes.
+        """
+
+        all_att = list(cls.__dict__.keys())
+        if not only_added:
+            return all_att
+        else:
+            default_atts = ['__module__', '__doc__', '__dict__', '__weakref__']
+            all_att = [x for x in all_att if x not in default_atts]
+            return all_att
+
     def OS(self):   
         """Checks the OS type and returns corresponding slash."""
         
@@ -40,27 +63,27 @@ class directories():
         directory = str(self.d)
         pt = ('%s*' %pattern)   # pattern.
         cont = os.listdir(directory)
-              
+
         for dir in cont:
             if fnmatch.fnmatch(dir, pt):
                 x = str(os.path.join(directory, dir))
-        
+
         return x
     
     def contents(self, dir, pattern, extension):
         """Finds a file according to a pattern and returns it as a string.
-        
+
         `dir` is the directory that will be searched.
-        
+
         `pattern` is the file name pattern.
-        
-        `extension` is the file extension. Add a . in the beggining of the string like: .txt"""
-        
+
+        `extension` is the file extension. Add a . in the beginning of the string like: .txt"""
+
         cont = os.listdir(dir)
         pt = ('%s*' %pattern)   # pattern.
-                
+
         for file in cont:
             if file.endswith(extension) and fnmatch.fnmatch(file, pt):
                 x = str(os.path.join(dir, file))
-                
+
         return x
