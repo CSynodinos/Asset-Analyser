7122eadffe5e72fd2c9511d1c1166d916c047cf7
Christos Synodinos <chris_sinodinos@outlook.com>
2022-06-13 15:58:37 +0100

    return prediction result from prediction function

--- lib/model_methods.py
+++ lib/model_methods.py
@@ -9,56 +9,57 @@ import logging
 logging.getLogger('tensorflow').disabled = True     # Disable Tensorflow warning messages.
 
 from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, LSTM
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, LSTM
 
-def preprocessing(data, prediction_days):
+def preprocessing(data, prediction_days) -> tuple[np.ndarray, np.ndarray, MinMaxScaler]:
     """Preprocess the training data.
-    
+
     `data` is the dataframe holding the data.
-    
+
     `prediction_days` are the number of days that the prediction will be based on.
-    
+
     Returns 3 variables: 
-    
+
     * `x_train`
-    
+
     * `y_train`
-    
-    * `scaler`"""
-    
+
+    * `scaler`
+    """
+
     scaler = MinMaxScaler(feature_range = (0, 1))
     scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1, 1))
-    
+
     x_train = []
     y_train = []
-    
+
     for i in range(prediction_days, len(scaled_data)):  # Loop over data according to the prediction days integer.
         x_train.append(scaled_data[i - prediction_days:i, 0])
         y_train.append(scaled_data[i, 0])
-    
+
     x_train, y_train = np.array(x_train), np.array(y_train)
     x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-    
+
     return x_train, y_train, scaler
 
 def test_preprocessing(prediction_days, inputs):
     """Preprocess the test data.
-    
+
     `prediction_days` are the number of days that the prediction will be based on.
-    
+
     `inputs` is the dataset being tested.
-    
+
     Returns `x_test`"""
-    
+
     x_test = []
-    
+
     for i in range(prediction_days, len(inputs)):
         x_test.append(inputs[i-prediction_days:i, 0])
-    
+
     x_test = np.array(x_test)
     x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
-    
+
     return x_test
 
 def RNN_model(x, y, units, closing_value, optimize, loss_function, epoch, batch):
@@ -86,19 +87,18 @@ def RNN_model(x, y, units, closing_value, optimize, loss_function, epoch, batch)
     model.add(LSTM(units = units))
     model.add(Dropout(0.2))
     model.add(Dense(units = closing_value)) # Predict a closing value. 1 is the next closing value.
-    
     model.compile(optimizer = optimize, loss = loss_function)
     model.fit(x, y, epochs = epoch, batch_size = batch, verbose = 0)
-    
+
     return model
 
 def plot_data(name, type, actual, predicted, colour_actual, colour_predicted):
     """Plot two arrays. `actual` and `predicted` are the two arrays being plotted.
-    
+
     `name` and `type` are the name of the data and type of data, respectively.
-    
+
     `colour_actual` and `colour_predicted` are the colours for `actual` and `predicted` lines in the plot, respectively."""
-    
+
     plt.plot(actual, color = colour_actual, label = '%s Actual Price' %name)
     plt.plot(predicted, color = colour_predicted, label = '%s Predicted Price' %name)
     plt.title('%s %s Price' %(name, type))
@@ -106,6 +106,8 @@ def plot_data(name, type, actual, predicted, colour_actual, colour_predicted):
     plt.ylabel('%s %s Price' %(name, type))
     plt.legend()
     plt.show()
+
+    return True
 
 def plot_volatility(dataframe, name):
     """Plot the volatility histogram.
@@ -121,7 +123,8 @@ def plot_volatility(dataframe, name):
     ax.set_xlabel("Log Volatility")
     ax.set_ylabel("Volatility Frequency(%)")
     ax.set_title("%s Volatility Plot" %name)
-    
+
+    return True
 
 def next_day_prediction(input, name, type, prediction_days, model, scaler, today = True, year = "", month = "", day = ""):
     """Predict the closing value that the array will have on the next day.
@@ -143,7 +146,7 @@ def next_day_prediction(input, name, type, prediction_days, model, scaler, today
     next_day = np.asarray(next_day).astype(np.float)
     prediction = model.predict(next_day)
     prediction = scaler.inverse_transform(prediction)
-    
+
     # Print the result and the prediction date.
     tomorrow = dt.date.today() + dt.timedelta(days=1)   # Today.
     if today == True:
@@ -154,3 +157,5 @@ def next_day_prediction(input, name, type, prediction_days, model, scaler, today
         print("%s %s Adj.Close price prediction (%d/%d/%d): $%f" %(name, type, day, month, year, prediction))
     else:
         print("%s %s Adj.Close price prediction: $%f" %(name, type, prediction))
+
+    return prediction
