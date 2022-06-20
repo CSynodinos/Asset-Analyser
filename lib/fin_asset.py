29219f8f5d6185dbc1bcb33fa88ffc2a84d6bd2d
Christos Synodinos <chris_sinodinos@outlook.com>
2022-06-20 11:23:12 +0100

    added database integretion for prediction prices

--- lib/fin_asset.py
+++ lib/fin_asset.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 from __future__ import annotations
+from re import A
+import shutil
 
 from sklearn.preprocessing import MinMaxScaler
 from dataclasses import dataclass
@@ -9,6 +11,8 @@ import yfinance as yf
 import datetime as dt
 import pandas as pd
 import numpy as np
+import sqlite3
+import os
 
 class financial_assets:
     """Financial asset class for price predictions.
@@ -49,7 +53,7 @@ class financial_assets:
         cols = ['Dates', 'Real_Values', 'Predicted_Values']
         return pd.DataFrame({cols[0]: d, cols[1]: real, cols[2]: pred})
 
-    def predictor(self, x, x_train: np.ndarray, y_train: np.ndarray, asset_scaler: MinMaxScaler,
+    def predictor(self, asset_n: str, x, x_train: np.ndarray, y_train: np.ndarray, asset_scaler: MinMaxScaler,
                 tick: str, query_asset: pd.DataFrame) -> tuple[float, str]:
 
         """Financial asset predictor.
@@ -107,6 +111,16 @@ class financial_assets:
         volat = str(percentage_vol(volatility))
         plot_volatility(asset_copy['Log returns'], name = tick)
         print(f'{tick} {self.asset_type} Volatility = {volat}%%')
+
+        # Send all_data to new database.
+        dbname = "Prediction_Assessment.db"
+        engine = sqlite3.connect(dbname)
+        if "-" in asset_n:
+            asset_n = asset_n.replace('-', "_")
+        if " " in asset_n:
+            asset_n = asset_n.replace(' ', "")
+
+        all_data.to_sql(asset_n, con = engine, if_exists = 'append', index = True)
 
         return all_data, next_day[0][0], volat
 
