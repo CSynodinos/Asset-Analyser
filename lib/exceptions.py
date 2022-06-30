743397a1e5adb6f795b6f5169559421e51c4f750
Christos Synodinos <chris_sinodinos@outlook.com>
2022-06-30 13:07:33 +0300

    New custom EntryNotFoundError error added for db

--- lib/exceptions.py
+++ lib/exceptions.py
@@ -19,3 +19,20 @@ class DateError(Exception):
             return '{0} '.format(self.errmessage)
         else:
             return 'DateError has been raised'
+
+class EntryNotFoundError(Exception):
+    """Custom exception class raised when a database entry is not found."""
+
+    __module__ = 'builtins'
+
+    def __init__(self, *args) -> None:
+        if args:
+            self.errmessage = args[0]
+        else:
+            self.errmessage = None
+
+    def __repr__(self) -> str:
+        if self.errmessage:
+            return '{0} '.format(self.errmessage)
+        else:
+            return 'EntryNotFoundError has been raised'
