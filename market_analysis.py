fac14e56876bf5f0ad56f4de74116ee77d4dce68
Christos Synodinos <chris_sinodinos@outlook.com>
2022-08-10 13:54:45 +0100

    utils for argparse, correct types of args in class

--- market_analysis.py
+++ market_analysis.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 from __future__ import annotations
+from asyncio import constants
 
 """Wrapper script for all modules and functionalities of the market analysis software.
 """
 
 import os, shutil, re
-from lib.args import args_parser
+import argparse
 from lib.data import data
 from lib.exceptions import AssetTypeError, PredictionDaysError, BadPortError, NoParameterError, DateError
 from lib.model_methods import preprocessing
@@ -16,16 +17,77 @@ import datetime as dt
 from inspect import getfullargspec
 from typing import Any, Final
 
+def yml_parser(f: str) -> dict:
+    """Parser for a .yml file.
+
+    Args:
+        * `f` (str): .yml file.
+
+    Returns:
+        dict: Contents of yml.
+    """
+
+    import yaml
+    with open(f, 'r') as fl_stream:
+        try:
+            return dict(yaml.safe_load(fl_stream))
+        except yaml.YAMLError as exc:
+            print(exc)
+
 # constants
-HELP_MESSAGE: Final[str] = 'Temporary Help message.'
-PORT: Final[int] = 8050
-PREDICTION_DAYS: Final[int] = 60
+parse = yml_parser(f = 'setup.yml')['constants']    # get constants.
+HELP_MESSAGE: Final[str] = parse['HELP_MESSAGE']
+PORT: Final[int] = parse['PORT']
+PLT: Final[bool] = parse['PLT']
+PREDICTION_DAYS: Final[int] = parse['PREDICTION_DAYS']
+
+def args_parser(msg) -> argparse.Namespace:
+    """Custom argument parser.
+    Args:
+        * `msg` (str): Description help message.
+    Returns:
+        `argparse.Namespace`: Namespace of input arguments.
+    """
+
+    parser = argparse.ArgumentParser(description = msg, formatter_class = argparse.RawDescriptionHelpFormatter)
+    parser.add_argument("-ast", help = "Market Asset. Needs to be written as: Asset_initials-currency")
+    parser.add_argument("-tp", help = "Asset Type, etc. Cryptocurrency or Stock.")
+    parser.add_argument("-pd", help = "Prediction days used for training. Must be a positive integer.")
+    parser.add_argument("-db", help = "Optional argument: SQLite3 Database name. Defaults: asset_name_data.db")
+    parser.add_argument("-d", help = "Optional argument: Start date for data calls. Format. Defaults: 2019-11-1")
+    parser.add_argument("-tdy", help = "Optional argument: End date for data calls is current date. If False, add custom date with format: YYYY-MM-DD. Defaults: True")
+    parser.add_argument("-p", help = "Optional argument: Port for localhost containing the dashboard. Defaults to 8050")
+    parser.add_argument("-plt", help = "Optional argument: Display seaborn plots. Useful for jupyter notebooks. Defaults to False")
+    return parser.parse_args()
+
+def bool_parser(var: any) -> bool:
+    """Check if parameter is boolean, if not, convert it to boolean.
+    Args:
+        * `var` (Any): variable to check for boolean.
+    Raises:
+        TypeError: Unable to convert to boolean.
+    Returns:
+        boolean: True if var is boolean, False if not.
+    """
+
+    _true = ["true", "True", "1"]
+    _false = ["false", "False", "0", None]
+    if type(var) == bool:
+        return var
+    else:
+        if var in _true:
+            return True
+        elif var in _false:
+            return False
+        else:
+            raise TypeError(f"{var} must be true, True, 1, False, false, 0 or None.")
 
 def _defaults(var: Any, default: object) -> object | Any:
     """Checks for None for variable var.
 
     Args:
         * `var` (Any): The variable.
+        * `var_type` (Any): Specify var type.
         * `default` (object): The default value of the object.
 
     Returns:
@@ -47,11 +109,12 @@ class analyze_asset:
 
     cwd: str = os.getcwd()
 
-    def __init__(self, asset_type: str, asset: str, big_db = None,
-                date = None, today = True,
-                pred_days = 60, port = 8050) -> None:
+    def __init__(self, asset_type: str, asset: str, big_db: str | None,
+                date: None | dt.datetime, today: bool,
+                pred_days: int, port: int, plt: bool) -> None:
 
         self.date = _defaults(var = self.date, default = dt.datetime(2019, 11, 1))
+
         self.asset_type = asset_type
         asset_types_tup = ('Cryptocurrency', 'cryptocurrency', 'crypto',
                         'Crypto', 'stock', 'Stock')
@@ -61,17 +124,27 @@ class analyze_asset:
         else:
             raise AssetTypeError(f"Asset type: {self.asset_type} is not valid. Some valid asset types are: "
                                 "Cryptocurrency, crypto, stock.")
+
         self.asset = asset
         valid_curr = ('USD', 'EUR', 'JPY', 'GBP')
         if not any(curr in self.asset for curr in valid_curr):
             self.asset = self.asset + "-USD"
-        self.today = today
+
+        self.today = _defaults(var = today, default = True)
+
         self.pred_days = _defaults(val = pred_days, default = PREDICTION_DAYS)
         if not isinstance(self.pred_days, int):
             raise PredictionDaysError('Prediction days used are not an integer.')
+        else:
+            self.pred_days = int(self.pred_days)
+
         self.port = _defaults(var = port, default = PORT)
         if not isinstance(port, int):
             raise BadPortError(f"Local host port: {port} is not an integer.")
+        else:
+            self.port = int(self.port)
+
+        self.plt = _defaults(val = self.plt, default = PLT)
 
     @classmethod
     def __repr__(cls) -> str:
@@ -124,13 +197,14 @@ class analyze_asset:
         asset_l_q = asset_l[0].replace("-", "_")
         asset_df, asset_dates = SQLite_Query(db_output_fl, asset_l_q)
         asset_x_train, asset_y_train, asset_scaler = preprocessing(asset_df, self.pred_days)
-        asset_class = financial_assets(pred_days = self.pred_days, asset_type = self.asset_type)
+        asset_class = financial_assets(pred_days = self.pred_days, asset_type = self.asset_type, plot = self.plt)
         asset_all_data, asset_next, asset_volatility = asset_class.predictor(x = asset_dates, 
                                                                             x_train = asset_x_train, 
                                                                             y_train = asset_y_train,
                                                                             asset_scaler = asset_scaler,
                                                                             tick = asset_l[0], 
                                                                             query_asset = asset_df)
+
         prediction_db = f"Prediction_Assessment_{self.asset}.db"
         prediction_assessment(df = asset_all_data, db = prediction_db, asset = asset_l[0])
         db_pred_new_fl = os.path.join(db_subdir, prediction_db)
@@ -142,6 +216,27 @@ class analyze_asset:
                         volatility = asset_volatility, port = self.port)
         return True
 
+def _dt_format(date: str | None):
+    """Checks for data format with regex. Format is YYYY-MM-DD.
+
+    Args:
+        * `date` (str | None): Input date.
+
+    Raises:
+        * `DateError`: If format is not YYYY-MM-DD.
+        * `DateError`: If input date is not a date.
+    """
+
+    r = re.compile('.*-.*-.*')
+    if date is not None:
+        if r.match(date) is not None:
+            try:
+                str(dt.datetime.strptime(date, '%Y-%m-%d').date())
+            except ValueError:
+                raise DateError('Date format should be YYYY-MM-DD.')
+        else:
+            raise DateError('Input for argument -d is not a date.')
+
 def main():
     args = args_parser(msg = HELP_MESSAGE)
     arguments = vars(args)
@@ -152,8 +247,10 @@ def main():
     pd: str = arguments.get('pd')
     pd_n: str = [k for k, v in locals().items() if v == pd][0]
     db: str | None = arguments.get('db')
-    p = str(arguments.get('p'))
+    tdy: bool | None= bool_parser(arguments.get('tdy'))
+    p: str = str(arguments.get('p'))
     d: str | None = arguments.get('d')
+    plt: bool = bool_parser(arguments.get('plt'))
 
     def _var_n(var_n, var: Any) -> tuple:
         if var == None:
@@ -176,17 +273,12 @@ def main():
         else:
             continue
 
-    # Check date format
-    r = re.compile('.*-.*-.*')
-    if d is not None:
-        if r.match(d) is not None:
-            try:
-                str(dt.datetime.strptime(d, '%Y-%m-%d').date())
-            except ValueError:
-                raise DateError('Date format should be YYYY-MM-DD.')
-        else:
-            raise DateError('Input for argument -d is not a date.')
-    #analyze_asset(asset_type = 'Cryptocurrency', asset = 'XRP', pred_days = 60).analyze()
+    # Date checks
+    _dt_format(date = d)
+    _dt_format(date = tdy)
+
+    analyze_asset(asset_type = 'Cryptocurrency', asset = 'XRP', big_db = db, date = d, pred_days = pd, port = p).analyze()
 
 if __name__ == "__main__":
-    main()
+    print(HELP_MESSAGE)
+    #main()
