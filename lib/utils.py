2aa4c5d552c6f47d926803eb75b26bb1c50aec4b
CSynodinos <chris_sinodinos@outlook.com>
2022-08-15 01:08:28 +0100

    terminal_str_formatter added

--- lib/utils.py
+++ lib/utils.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from __future__ import annotations
 
-import yaml
+import yaml, os
 
 def yml_parser(f: str) -> dict:
     """Parser for a .yml file.
@@ -18,3 +18,15 @@ def yml_parser(f: str) -> dict:
             return dict(yaml.safe_load(fl_stream))
         except yaml.YAMLError as exc:
             print(exc)
+
+def terminal_str_formatter(_str_: str) -> str:
+    """Bold and centre a string for the terminal. 
+
+    Args:
+        * `_str_` (str): Input str.
+
+    Returns:
+        `str`: Formatted str.
+    """
+    terminal_width = os.get_terminal_size().columns
+    return f'\033[1m{_str_}\033[0m'.center(terminal_width)
